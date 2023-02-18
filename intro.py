c12ca0ce9f6365e92be61db493fc4d62c3b4b85b
Diego Guzmán <oropezaeduardo442@gmail.com>
2023-02-18 01:35:54 -0600

    Aprendiendo a usar Tkinter

---
+++ intro.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+root = Tk()
+root.title('Hola mundo')
+root.geometry('500x500')
+
+label = Label(root, text='Hola mundo, este es mi primera etiqueta!').pack()
+
+root.mainloop()
